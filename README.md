adac0c4116c137eef48fb5a521287c9760f46706
Marius Mitrofan <mariusmitrofan@cloud.upwork.com>
2017-03-27 19:29:15 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# percona-cluster-wordpress_with_HAProxy
+# percona-cluster-wordpress
 Deploy a Percona cluster behind a HA proxy to which connect 3 wordpress containers that have a HA proxy in front of them.
+
+This tutorial has been created using `Docker under Windows 10 with Hyper-V`.
+Please amend the volumes path according to your OS.
+
+## CLEAR WORKSPACE
+```
+docker rm $(docker ps -a -q) -f
+docker rmi $(docker images -a -q) -f
+```
+
+### CREATE LOGICAL NETWORKS
+```
+docker network create backend
+docker network create frontend
+```
+
+## LAUNCH DATABASE NODES
+```
+docker run --name db1 -d -p 3306 --net=backend  -e MYSQL_ROOT_PASSWORD=root  -e CLUSTER_NAME=cluster  -e XTRABACKUP_PASSWORD=root percona/percona-xtradb-cluster
+docker run --name db2 -d -p 3306 --net=backend --link db1:db1 -e MYSQL_ROOT_PASSWORD=root  -e CLUSTER_NAME=cluster -e CLUSTER_JOIN=db1  -e XTRABACKUP_PASSWORD=root percona/percona-xtradb-cluster
+docker run --name db3 -d -p 3306 --net=backend --link db1:db1 -e MYSQL_ROOT_PASSWORD=root  -e CLUSTER_NAME=cluster -e CLUSTER_JOIN=db1  -e XTRABACKUP_PASSWORD=root percona/percona-xtradb-cluster
+```
+
+## LAUNCH DB PROXY
+```
+docker run -d --name proxydb -v C:/Users/mariu/Documents/Training/percona-wordpress/proxydb:/usr/local/etc/haproxy:ro -d -p 3306:3306 --net=backend --link db1:db1 --link db2:db2 --link db3:db3 haproxy:1.7
+```
+
+## POPULATE DATABASE
+```
+docker run --name web1 -d -p 80:80 --net=backend --link db1:db1 -e WORDPRESS_DB_HOST=db1 -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD=root -e WORDPRESS_DB_NAME=wordpress -e WORDPRESS_TABLE_PREFIX=wp wordpress
+```
+
+<< finish and install >>
+COPY OUTPUT OF << docker exec -ti web1 cat /var/www/html/wp-config.php >> TO  .\percona-wordpress\wordpress\wp-config.php
+
+## BUILD WEB DOCKER
+```
+docker build -t web .\percona-wordpress\wordpress
+```
+
+## LUNACH WEB NODES
+```
+docker run --name web1 -d -p 80 --net=frontend --link proxydb:db1 -v C:/Users/mariu/Documents/Training/galera-wordpress/wordpress/uploads:/var/www/html/wp-content/uploads:rw  web
+docker run --name web2 -d -p 80 --net=frontend --link proxydb:db1 -v C:/Users/mariu/Documents/Training/galera-wordpress/wordpress/uploads:/var/www/html/wp-content/uploads:rw  web
+docker run --name web3 -d -p 80 --net=frontend --link proxydb:db1 -v C:/Users/mariu/Documents/Training/galera-wordpress/wordpress/uploads:/var/www/html/wp-content/uploads:rw  web
+```
+
+## ATTACH WEB NODES TO BACKEND NETWORK
+```
+docker network connect backend web1
+docker network connect backend web2
+docker network connect backend web3
+```
+
+## LAUNCH WEB PROXY
+```
+docker run -d --name proxyweb --net=frontend -v C:/Users/mariu/Documents/Training/percona-wordpress/proxyweb:/usr/local/etc/haproxy:ro -d -p 80:80  --link web1:web1 --link web2:web2 --link web3:web3 haproxy:1.7
+```
---
+++ percona-wordpress/wordpress/wp-config.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+define('DB_HOST', 'db1');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '98a1884c5b3c52cf6d307e935ca16fdaba54fae1');
+define('SECURE_AUTH_KEY',  '30cd2831c9bb1d484be431e047402cc09f24546c');
+define('LOGGED_IN_KEY',    '3d058526dcebf86775e55bce9550a6ea9aa2cf5b');
+define('NONCE_KEY',        'b28fd367be9b228b37d3dcea2908ae52f2efb2fe');
+define('AUTH_SALT',        '83b41ab2c057ca561b9bfb88195f235f69ff18be');
+define('SECURE_AUTH_SALT', '6f5f1051036a0d32e107c4bf3615a0f5d2afe37a');
+define('LOGGED_IN_SALT',   '07fe3e1e5b8c2127ef1cfb0688e7024552308184');
+define('NONCE_SALT',       '922ac3b330a1d84f5b6139a16a7dd4d38c6e0cef');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+        $_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+        define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
